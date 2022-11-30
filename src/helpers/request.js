0d46df37b45f8749ca6b0b8cace055e141c2e0e0
Jackeybai <1303802862@qq.com>
2022-11-30 17:15:13 +0800

    修改https

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -4,8 +4,11 @@ axios.defaults.headers.post["Content-Type"] =
   "application/x-www-form-urlencoded";
 // 对于所有的post请求统一设置Content-Type
 
-axios.defaults.baseURL = "http://note-server.hunger-valley.com";
+axios.defaults.baseURL = "https://note-server.hunger-valley.com";
 // 接口的url
+
+axios.defaults.withCredentials = true;
+// 设置跨域
 
 export default function request(url, type = "GET", data = {}) {
   return new Promise((resolve, reject) => {
@@ -26,10 +29,12 @@ export default function request(url, type = "GET", data = {}) {
         if (res.status === 200) {
           resolve(res.data);
         } else {
+          console.error(res.data);
           reject(res.data);
         }
       })
       .catch(err => {
+        console.error({ msg: "网络异常" });
         reject({ msg: "网络异常" });
       });
   });
