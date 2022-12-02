bff16d3da06620f2732da7f931fcec2bb02e464c
Jackeybai <1303802862@qq.com>
2022-12-02 18:14:40 +0800

    笔记本列表页功能完善

--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="detail" id="notebook-list">
     <header>
-      <a href="#" class="btn"><i class="iconfont icon-plus"></i> 新建笔记本</a>
+      <a href="#" class="btn" @click.prevent="onCreate"><i class="iconfont icon-plus"></i> 新建笔记本</a>
     </header>
     <main>
       <div class="layout">
-        <h3>笔记本列表0</h3>
+        <h3>笔记本列表({{notebooks.length}})</h3>
         <div class="book-list">
-          <router-link to="/note/1" class="notebook">
+          <router-link v-for="notebook in notebooks" to="/note/1" class="notebook">
             <div>
-              <span class="iconfont icon-notebook"></span> 笔记本
-              <span>2</span>
-              <span class="action">编辑</span>
-              <span class="action">删除</span>
-              <span class="date">3</span>
+              <span class="iconfont icon-notebook"></span> {{notebook.title}}
+              <span>{{notebook.cont}}</span>
+              <span class="action" @click.prevent="onEdit(notebook)">编辑</span>
+              <span class="action" @click.prevent="onDelete(notebook)">删除</span>
+              <span class="date">{{notebook.friendlyCreatedAt }}</span>
             </div>
           </router-link>
         </div>
@@ -28,13 +28,14 @@
 import Auth from '@/apis/auth';
 import router from '@/router/index'
 import Notebook from '@/apis/notebooks'
-window.Notebooks = Notebook
+import { friendlyDate } from '@/helpers/util'
+
 
 export default {
-  name: 'Login',
   data() {
     return {
-      msg: '笔记本列表'
+      msg: '笔记本列表',
+      notebooks: [],
     }
   },
   created() {
@@ -44,6 +45,48 @@ export default {
           router.push({ path: '/login' })
         }
       })
+    Notebook.getAll()
+      .then(res => {
+        this.notebooks = res.data
+      })
+  },
+  methods: {
+    onCreate() {
+      let title = window.prompt('创建笔记本')
+      if (title.trim() === '') {
+        alert('笔记本名不能为空')
+        return
+      }
+      Notebook.addNotebook({ title })
+        .then(res => {
+          console.log(res.msg);
+          res.data.friendlyCreatedAt = friendlyDate(res.data.createdAt)
+          this.notebooks.unshift(res.data)
+          alert(res.msg)
+        })
+    },
+    onEdit(notebook) {
+      let title = window.prompt('修改标题', notebook.title)
+      Notebook.updateNotebook(notebook.id, { title })
+        .then(res => {
+          console.log(res);
+          alert(res.msg)
+          notebook.title = title
+        })
+    },
+    onDelete(notebook) {
+      let isConfirm = window.confirm('确定要删除笔记本吗？')
+      console.log(isConfirm);
+      if (isConfirm) {
+        Notebook.deleteNotebook(notebook.id)
+          .then(res => {
+            console.log(res);
+            alert(res.msg)
+            this.notebooks.splice(this.notebooks.indexOf(notebook), 1)
+          })
+      }
+
+    }
   }
 }
 </script>
