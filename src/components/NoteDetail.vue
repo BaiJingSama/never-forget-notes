a21596a0e3a06634b9cdc547df7227231d24ecee
Jackeybai <1303802862@qq.com>
2022-12-06 15:19:41 +0800

    完成笔记编辑区结构和样式

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -1,9 +1,21 @@
 <template>
   <div id="note" class="detail">
     <Note-sidebar />
-    <div id="note-detail">
-      <h1>notebookId : {{ $route.query.notebookId }}</h1>
-      <h2>noteId : {{ $route.query.noteId }}</h2>
+    <div class="note-detail">
+      <div class="note-bar">
+        <span>创建日期：{{curNote.createdAtFriendly}}</span>
+        <span>更新日期：{{curNote.updatedAtFriendly}}</span>
+        <span>{{curNote.statusText}}</span>
+        <span class="iconfont icon-delete"></span>
+        <span class="iconfont icon-fullscreen"></span>
+      </div>
+      <div class="note-title">
+        <input type="text" :value="curNote.title" placeholder="输入标题">
+      </div>
+      <div class="editor">
+        <textarea v-show="true" :value="curNote.context" placeholder="输入内容，支持markdown语法"></textarea>
+        <div class="preview markdown-body" v-show="false"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -16,7 +28,13 @@ export default {
   name: 'Login',
   data() {
     return {
-      msg: '笔记本详情列表'
+      curNote: {
+        title: '我的笔记',
+        context: '我的笔记内容',
+        createdAtFriendly: '1天前',
+        updatedAtFriendly: '刚刚',
+        statusText: '未更新'
+      }
     }
   },
   created() {
@@ -33,7 +51,9 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="less">
+@import url(../assets/css/note-detail.less);
+
 #note {
   display: flex;
   align-items: stretch;
